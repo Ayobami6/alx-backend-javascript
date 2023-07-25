f458b696cb0c7b5f2a87609d2cd79910b9af5192
Ayobami <ayobamidele006@gmail.com>
2023-07-25 04:44:08 +0100

    replace for -n... loop with for of in function
    Used for of loop ES6 instead to append to each array values

--- 0-main.js
+++ 0-main.js
@@ -1,6 +1,5 @@
-import getFullBudgetObject from './0x00-ES6_basic/9-getFullBudget';
+import appendToEachArrayValue from './0x00-ES6_basic/10-loops';
 
-const fullBudget = getFullBudgetObject(20, 50, 10);
-
-console.log(fullBudget.getIncomeInDollars(fullBudget.income));
-console.log(fullBudget.getIncomeInEuros(fullBudget.income));
+console.log(
+  appendToEachArrayValue(['appended', 'fixed', 'displayed'], 'correctly-'),
+);
