72fb15ebb5d3e56c74f2ea96a0ad6aa886041efc
Ayobami <ayobamidele006@gmail.com>
2023-07-25 20:05:58 +0100

    add catch to the multiple promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,11 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const photoPromise = uploadPhoto();
   const createUserPromise = createUser();
-  Promise.all([photoPromise, createUserPromise]).then((data) => {
-    console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
-  });
+  Promise.all([photoPromise, createUserPromise])
+    .then((data) => {
+      console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
