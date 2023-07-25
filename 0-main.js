ab3e2e4008e8a57bad0c5c1f1a8a38c12b9e60da
Ayobami <ayobamidele006@gmail.com>
2023-07-25 05:29:53 +0100

    mandatory tasks completed

--- 0-main.js
+++ 0-main.js
@@ -1,3 +1,13 @@
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable import/no-named-as-default-member */
+import createReportObject from './0x00-ES6_basic/12-createReportObject';
 import createEmployeesObject from './0x00-ES6_basic/11-createEmployeesObject';
 
-console.log(createEmployeesObject('Software', ['Bob', 'Sylvie']));
+const employees = {
+  ...createEmployeesObject('engineering', ['Bob', 'Jane']),
+  ...createEmployeesObject('marketing', ['Sylvie']),
+};
+
+const report = createReportObject(employees);
+console.log(report.allEmployees);
+console.log(report.getNumberOfDepartments(report.allEmployees));
