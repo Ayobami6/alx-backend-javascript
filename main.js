56e166deed9cb18ed6df3b8656a572631a4f69c4
Ayobami <ayobamidele006@gmail.com>
2023-07-26 12:59:38 +0100

    reject photo promise

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-import signUpUser from './0x01-ES6_promise/4-user-promise';
+import uploadPhoto from './0x01-ES6_promise/5-photo-reject';
 
-console.log(signUpUser('Bob', 'Dylan'));
+console.log(uploadPhoto('guillaume.jpg'));
