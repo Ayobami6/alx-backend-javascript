58e058fc664edce605a9b7ce30a9c830a147fde6
Ayobami <ayobamidele006@gmail.com>
2023-07-27 16:10:58 +0100

    change attribute order in currency class

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-underscore-dangle */
 export default class Currency {
   constructor(code, name) {
-    this._name = name;
     this._code = code;
+    this._name = name;
   }
 
   get code() {
