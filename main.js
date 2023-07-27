fd6236264722ce611dbebe012933c9fdaad11d31
Ayobami <ayobamidele006@gmail.com>
2023-07-27 18:43:11 +0100

    ABC first implementation

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-import Pricing from './0x02-ES6_classes/4-pricing';
-import Currency from './0x02-ES6_classes/3-currency';
+import Building from './0x02-ES6_classes/5-building';
 
-const p = new Pricing(100, new Currency('EUR', 'Euro'));
-console.log(p);
-console.log(p.displayFullPrice());
+const b = new Building(100);
+console.log(b);
+
+class TestBuilding extends Building {}
+
+try {
+  new TestBuilding(200);
+} catch (err) {
+  console.log(err);
+}
