2b92ba4e3065c59661cc9316f908e4cf52f0e69a
Ayobami <ayobamidele006@gmail.com>
2023-08-01 16:44:27 +0100

    implement get students by ids&sum ids

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-import getListStudentIds from './0x03-ES6_data_manipulation/1-get_list_student_ids';
 import getListStudents from './0x03-ES6_data_manipulation/0-get_list_students';
+import getStudentIdsSum from './0x03-ES6_data_manipulation/3-get_ids_sum';
 
-console.log(getListStudentIds('hello'));
-console.log(getListStudentIds(getListStudents()));
+const students = getListStudents();
+const value = getStudentIdsSum(students);
+
+console.log(value);
