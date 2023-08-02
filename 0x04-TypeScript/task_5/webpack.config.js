1e8040258390a01c796925438779f1f5436edd3f
Ayobami <ayobamidele006@gmail.com>
2023-08-02 23:51:21 +0100

    Minor&Major credits interface implementation

--- 0x04-TypeScript/task_5/webpack.config.js
+++ 0x04-TypeScript/task_5/webpack.config.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
-  entry: './task_5/js/main.ts',
+  entry: './task_4/js/main.ts',
   devtool: 'inline-source-map',
   module: {
     rules: [
