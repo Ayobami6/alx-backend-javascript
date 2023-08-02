bd2c1364c9b8671828819d1bc370b6f99e7f4c13
Ayobami <ayobamidele006@gmail.com>
2023-08-02 17:04:41 +0100

    task_0&task_1 student interface implemented

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,12 @@
+interface DirectorInterface { 
+    workFromHome: () => string;
+    getCoffeeBreak: () => string;
+    workDirectorTasks: () => string;
+}
+
+interface TeacherInterface { 
+    workFromHome: () => string;
+    getCoffeeBreak: () => string;
+    workTeacherTasks: () => string;
+
+}
