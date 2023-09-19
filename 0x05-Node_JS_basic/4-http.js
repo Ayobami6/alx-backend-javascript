a057e0783fe4612bb51eeaef7d077e25a7fe4011
Ayobami <ayobamidele006@gmail.com>
2023-09-19 09:05:36 +0100

    change setHead to setHeader in task 04

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,7 +4,7 @@ const PORT = 1245;
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHead('Content-Type', 'text/plain');
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
