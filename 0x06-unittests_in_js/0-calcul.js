13176e095d1c1ae7536887f9531bc994bbe1c199
Ayobami Alaran A <ayobamidele006@gmail.com>
2023-09-20 20:10:50 +0100

    Add test for calculat number method

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,8 +1,7 @@
 const calculateNumber = (a, b) => {
 	const roundA = Math.round(a);
 	const roundB = Math.round(b);
-	const result = roundA + roundB;
-	return result;
+	return roundA + roundB;
 }
 
-export default calculateNumber;
+module.exports = calculateNumber;
