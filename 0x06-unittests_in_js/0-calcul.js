d2b2a3d9f330d05ac00b2801877483c01052ccf6
Ayobami Alaran A <ayobamidele006@gmail.com>
2023-09-21 08:52:00 +0000

    Thu, Sep 21, 2023, 9:52 AM +01:00

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,7 +1,7 @@
 const calculateNumber = (a, b) => {
-	const roundA = Math.round(a);
-	const roundB = Math.round(b);
-	return roundA + roundB;
-}
+  const roundA = Math.round(a);
+  const roundB = Math.round(b);
+  return roundA + roundB;
+};
 
 module.exports = calculateNumber;
