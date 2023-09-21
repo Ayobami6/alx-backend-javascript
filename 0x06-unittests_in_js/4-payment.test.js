a496e7ce3089203ae19e396771bd0f99576c9583
Ayobami <ayobamidele006@gmail.com>
2023-09-21 23:42:12 +0100

    add test for task4

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const { describe, it } = require('mocha');
+const expect = chai.expect;
+const assert = require('assert');
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment');
+
+describe('Test Utils calls args', () => {
+  it('should return cause Utils.calculate with 3 args', () => {
+    const spy = sinon.spy(console.log);
+    const stub = sinon.stub(Utils.calculateNumber).returns(10)    
+    sendPaymentRequestToApi(100, 20);
+    assert(spy.withArgs('The total is: 120').calledOnce)
+    assert(sub.withArgs("SUM", 100, 20).calledOnce)
+  });
+});
